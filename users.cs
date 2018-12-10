82c8304a917d877116980dba12cffed0f4de389d
AhmedKassem2019 <44648748+AhmedKassem2019@users.noreply.github.com>
2018-12-10 09:35:48 +0200

    Add files via upload

---
+++ users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace commerce.Models
+{
+    public class users
+{
+
+        public string Email;
+        public string password;
+
+        public static bool log(string Email, string password)
+        {
+            if (Email == "admin" && password == "admin")
+                return true;
+            else
+                return false;
+
+
+        }
+
+
+
+
+
+
+    }
+}
